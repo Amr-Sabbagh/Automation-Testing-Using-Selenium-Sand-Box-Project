df1560b2f8f7456ceb3cf9ed4f967c520e53d6eb
Amr Sabbagh <amr.sabbagh1@gmail.com>
2025-08-05 15:10:17 +0300

    updated project to utilize fluent design pattern with the pom

--- src/test/java/seleniumFluentPOMTest/FindingTheFirstSearchResultFluentPOMTest.java
+++ src/test/java/seleniumFluentPOMTest/FindingTheFirstSearchResultFluentPOMTest.java
@@ -11,8 +11,6 @@ import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import pomPages.DuckDuckGoHomePage;
-import pomPages.DuckDuckGoSearchResultPage;
 
 import java.time.Duration;
 
